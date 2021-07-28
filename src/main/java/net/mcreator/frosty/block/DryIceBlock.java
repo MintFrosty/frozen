87dc93e5238b06e3e7812a502327180191f42703
MintFrosty <wespa@192.168.1.10>
2021-07-28 16:22:16 -0400

    hi

---
+++ src/main/java/net/mcreator/frosty/block/DryIceBlock.java
@@ -0,0 +1,50 @@
+
+package net.mcreator.frosty.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.frosty.FrostyModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@FrostyModElements.ModElement.Tag
+public class DryIceBlock extends FrostyModElements.ModElement {
+	@ObjectHolder("frosty:dry_ice")
+	public static final Block block = null;
+	public DryIceBlock(FrostyModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.LAVA).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("dry_ice");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 2));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/frosty/item/SnowItem.java
@@ -0,0 +1,44 @@
+
+package net.mcreator.frosty.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+
+import net.mcreator.frosty.FrostyModElements;
+
+@FrostyModElements.ModElement.Tag
+public class SnowItem extends FrostyModElements.ModElement {
+	@ObjectHolder("frosty:snow")
+	public static final Item block = null;
+	public SnowItem(FrostyModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.UNCOMMON)
+					.food((new Food.Builder()).hunger(4).saturation(0.3f).build()));
+			setRegistryName("snow");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.DRINK;
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getEatSound() {
+			return net.minecraft.util.SoundEvents.ENTITY_GENERIC_DRINK;
+		}
+	}
+}
